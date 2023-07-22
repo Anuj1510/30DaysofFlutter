8321a512fe3bfaef17ed539f792c7cf74038da9a
Anuj1510 <anujshahi1510@gmail.com>
2023-07-22 16:46:19 +0530

    day 4 completed

--- lib/Login_page.dart
+++ lib/Login_page.dart
@@ -1,15 +1,63 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 40, color: Colors.amber, fontWeight: FontWeight.bold),
-        ),
+      color: Colors.white,
+      child: Column(
+        // column ya phir row tub use krte hai jub kaphi saari cheeze dalni hoti hai, isliye child ke badle hamne children ka use kiya
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Image.asset(
+            "assets/images/password.png",
+            height: 300,
+            width: 300,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Welcome",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter UserName",
+                    labelText: "UserName",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Enter password",
+                    labelText: "UserPassword",
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(),
+                  child: const Text("Login"),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
